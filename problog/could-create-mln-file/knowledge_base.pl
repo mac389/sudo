51f412134a07404182e82c101ca8a22c3782ac36
Michael Chary <mac389@gmail.com>
2019-11-02 21:01:36 -0400

    making knowledge base

--- problog/could-create-mln-file/knowledge_base.pl
+++ problog/could-create-mln-file/knowledge_base.pl
@@ -6,3 +6,37 @@ man(X) :-
 	person(X),
 	male(X).
 
+adolescent(X) :-
+	person(X).
+	\+adult(X),
+	\+child(X).
+
+pregnant_substance_user(X) :-
+	person(X),
+	pregnant(X),
+	uses(X,Y),
+	isSubstance(Y).
+
+isSubstance(X) :- 
+	%how to look up in list 
+	yse.
+
+illicit_drug_user(X) :-
+	person(X),
+	uses(X,Y),
+	isIllicitDrug(Y).
+
+use_stimulant(X) :-
+	person(X),
+	uses(X,Y),
+	isStimulant(Y).
+
+dependence(X,Y) :-
+	%defining for meaning of dependent on substance
+	person(X),
+	isSubstance(Y),
+	uses(X,Y). %feels like need more criteria, like the DSM criteria
+
+dependence_on_prescription_opioids(X) :-
+	dependence(X,Y),
+	isPrescriptionOpioid(Y).
