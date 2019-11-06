4924540d6e8f00f941d9ab163b5e772a44432c9d
Michael Chary <mac389@gmail.com>
2019-11-06 16:29:31 -0500

    Format all logics

--- src/reformat-axiomata.py
+++ src/reformat-axiomata.py
@@ -1,11 +1,21 @@
 import csv, os 
 
+from tqdm import tqdm 
+from pprint import pprint
+
 filename = os.path.join('..','jamia-axiomata.csv')
 contents = list(csv.DictReader(open(filename,'r'),delimiter='\t',skipinitialspace=True))
 
 PATH = os.path.join('..','problog','could-create-mln-file')
 
-for name in os.listdir(PATH):
+translation_table = {item['original']:item['converted'] for item in contents}
+
+for name in tqdm(os.listdir(PATH),'Formatting axiomata'):
 	if os.path.isdir(os.path.join(PATH,name)):
-		axioms = open(os.path.join(PATH,name,'abstract.logic'),'r')
-		
+		axioms = open(os.path.join(PATH,name,'abstract.logic'),'r').read()
+		for string in contents:
+			for string in translation_table:
+				if string in axioms:
+					axioms = axioms.replace(string,translation_table[string])
+		with open(os.path.join(PATH,name,'formatted.logic'), 'w') as out:
+			out.write(axioms)
