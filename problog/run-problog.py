cc309cb0ab2403a5b87a602cdabd2b3b51943841
Michael Chary <mac389@gmail.com>
2019-11-06 20:12:06 -0500

    copy edit

--- problog/run-problog.py
+++ problog/run-problog.py
@@ -8,12 +8,12 @@ from problog import get_evaluatable
 axioms = open(os.path.join('..','amalgamated.axiomata'),'r').read()
 print axioms 
 
-'''
-p = PrologString(axioms + """
+
+p = PrologString(axioms + """\n
 person(1).
-receive_initial_therapy(1,standard_intake_evaluation).
+receive(1,initial_therapy)
+receive(1,standard_intake_evaluation).
 query(stay_in_therapy(1,n84_day)).
 """)
 
 print get_evaluatable().create_from(p).evaluate()
-'''
