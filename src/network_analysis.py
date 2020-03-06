470dc2506bfed51c166ba8c13c818261ece32d56
Michael Chary <mac389@gmail.com>
2020-03-06 12:05:16 -0500

    update

--- src/network_analysis.py
+++ src/network_analysis.py
@@ -4,6 +4,7 @@ import networkx as nx
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt 
+import seaborn as sns 
 
 from tqdm import tqdm 
 
@@ -27,6 +28,30 @@ for one,two in itertools.combinations(db.keys(),2):
 	df.loc[one,two] = js
 	df.loc[two,one] = js
 
+
+
 G=nx.from_pandas_adjacency(df)
-nx.draw(G)
+weights = np.log([G[u][v]['weight'] for u,v in G.edges()])
+
+
+nx.draw(G, pos=nx.nx_agraph.pygraphviz_layout(G, prog='neato'), with_labels=True, node_size=1000, 
+		face_color='w', linewidths=1.5, edge_color=weights, edge_cmap=plt.cm.bone_r)
+plt.savefig(os.path.join('../social-network.png'),dpi=600,bbox_inches='tight')
+
+
+
+'''
+sns.heatmap(df)
 plt.show()
+'''
+'''
+pos = nx.get_node_attributes(G,'posxy')
+print(pos)
+### Output for tikz-network
+
+#Edges
+df = pd.DataFrame([[5*pos[key][0],5*pos[key][1],True] for key in db.keys()],
+		columns=['x','y','IdAsLabel'], index=db.keys())
+
+df.to_csv('../vertices.csv', index_label='id')
+'''
